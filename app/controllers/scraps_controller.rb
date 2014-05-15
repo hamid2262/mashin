406958ec40715063b55976b9f8816abf3517404e
hamid <hamid2262@yahoo.com>
2014-05-16 01:11:57 +0200

    make scrap conditional for production and development

--- app/controllers/scraps_controller.rb
+++ app/controllers/scraps_controller.rb
@@ -1,12 +1,14 @@
 class ScrapsController < ApplicationController
   def index
-    # system "rake scrap --trace 2>&1 >> #{Rails.root}/log/rake.log &"
-    scrap_url = "http://obscure-woodland-9401.herokuapp.com/"
-    scrap = Scrap.new
-    scrap.url = scrap_url+"ads"
-    scrap.save
-    scrap.sweep    
-    redirect_to root_path    
+    if Rails.env.production?
+      system "rake scrap --trace 2>&1 >> #{Rails.root}/log/rake.log &"
+    else
+      scrap_url = "http://obscure-woodland-9401.herokuapp.com/"
+      scrap = Scrap.new
+      scrap.url = scrap_url+"ads"
+      scrap.save
+      scrap.sweep    
+      redirect_to root_path    
+    end
   end
-
 end
