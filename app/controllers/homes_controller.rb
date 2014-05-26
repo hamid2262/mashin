f6f925d1241df631a6e3c54d2d1b8fa555cb7433
hamid <hamid2262@yahoo.com>
2014-05-26 14:15:06 +0200

    change car_model select field in search and home and add todays ads count to home

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,10 +1,9 @@
 class HomesController < ApplicationController
   load_and_authorize_resource
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => exception.message
-  end
 
   def show
     @search = Search.new
+    @ad_count = Home.todays_ads
+    @makes = Home.makes
   end
 end
--- app/models/home.rb
+++ app/models/home.rb
@@ -1,3 +1,16 @@
 class Home 
   
+  def self.todays_ads
+    Ad.where("updated_at > ?", beginning_of_today).count
+  end
+
+  def self.makes
+    makes = Make.joins(:ads).group(['makes.name'])
+    makes = makes.where("ads.updated_at > ?", Home.beginning_of_today)
+    makes.order('COUNT(ads.make_id) DESC').limit(15).count
+  end
+
+  def self.beginning_of_today
+    DateTime.now.beginning_of_day()
+  end
 end
