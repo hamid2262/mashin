787cfcb4d1607e06286c9502b4060445eddcab13
hamid <hamid2262@yahoo.com>
2014-09-07 21:54:08 +0200

    add car infos to built_year show page

--- app/helpers/built_years_helper.rb
+++ app/helpers/built_years_helper.rb
@@ -1,2 +1,10 @@
 module BuiltYearsHelper
+
+  def built_year_title built_year
+    title = " "
+    title = title + built_year.car_model.make.name + "، "
+    title = title + built_year.car_model.name    + "، "
+    title = title + built_year.year.to_s
+  end
+
 end
